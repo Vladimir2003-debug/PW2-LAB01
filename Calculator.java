69bec603e10c395fe227a2b730f40c55b84f894c
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-05-07 01:22:27 -0500

    creado metodo mod()

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,14 @@
 /*
  * Calculadora con metodos de clase
  */
-public class Calculadora {
-    
+public class Calculator {
+    /**
+     * Metodo que saca el modulo de un numero
+     * @param a : Dividendo
+     * @param a : Divisor
+     * @return Residuo
+     */
+    public static int mod(int a, int b) {
+       return a % b; 
+    }
 }
