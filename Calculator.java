4ba7a9e2332c3f74171b74d9bcf606fd3e20d6ff
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-05-07 01:14:27 -0500

    creado metodo mul()

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,14 @@
 /*
  * Calculadora con metodos de clase
  */
-public class Calculadora {
-    
+public class Calculator {
+    /**
+     * Metodo que multiplica dos numeros
+     * @param a : factor
+     * @param b : factor
+     * @return El producto de los numeros
+     */
+    public static int mul(int a, int b) {
+        return a * b;
+    }
 }
