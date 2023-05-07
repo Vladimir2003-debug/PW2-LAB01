e840cb51683830ae1f8950026556134f35a7e1b7
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-05-07 01:11:23 -0500

    creado metodo sub()

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,15 @@
 /*
  * Calculadora con metodos de clase
  */
-public class Calculadora {
-    
+public class Calculator {
+    /**
+     * Metodo que resta dos numeros
+     * @param a : Minuendo
+     * @param b : Sustraendo
+     * @return Diferencia
+     */
+    public static int sub(int a ,int b) {
+        return a-b;
+    }
+
 }
