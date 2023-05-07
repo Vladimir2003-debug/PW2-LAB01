5b47e7fe3644ab4fc00328ae691188ef2880c4be
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-05-07 01:08:25 -0500

    Creacion del metodo add()

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,13 @@
 /*
  * Calculadora con metodos de clase
  */
-public class Calculadora {
-    
+public class Calculator {
+    /**
+     * Metodo que suma dos numeros enteros
+     * @param a : primer sumando
+     * @param b : segundo sumando
+     */
+    public static int add(int a,int b) {
+        return a + b; 
+    }
 }
