24ce927913f46dba4b0eb02f9f343514ba25300d
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2023-05-07 01:23:35 -0500

    añadido return a add()

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
      * Metodo que suma dos numeros enteros
      * @param a : primer sumando
      * @param b : segundo sumando
+     * @return suma
      */
     public static int add(int a,int b) {
         return a + b; 
